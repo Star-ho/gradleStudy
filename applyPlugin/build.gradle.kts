bf50fabafe6030503fd7b63b4da1286d3edec1f0
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-04-02 11:26:07 +0900

    04-02

---
+++ applyPlugin/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins{
+    id("com.exaple.hello") version "1.0.0" apply false
+    id("com.exaple.byebye") version "1.0.0" apply false
+}
---
+++ applyPlugin/setting.gradle.kts
@@ -0,0 +1,3 @@
+include("hello-a")
+include("hello-b")
+include("hello-c")
